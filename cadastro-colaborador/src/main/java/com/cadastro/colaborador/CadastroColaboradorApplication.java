fd843e8faa2cc7a33d772985d19669b9b7f7f539
Victoria Rocha <victoriaspassatempo@gmail.com>
2022-06-13 09:21:30 -0300

    Adição do projeto

---
+++ cadastro-colaborador/src/main/java/com/cadastro/colaborador/CadastroColaboradorApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.colaborador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroColaboradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroColaboradorApplication.class, args);
+	}
+
+}
---
+++ cadastro-colaborador/src/test/java/com/cadastro/colaborador/CadastroColaboradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.colaborador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroColaboradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
